f74101ddb8601b30eb90dbaba1225a3e19211154
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-01-31 00:04:23 +0000

    🫦

--- TZT/jang/Program.cs
+++ TZT/jang/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿// Csharp Tezt 
+
+using Pastel;
+using System.Drawing;
+
+namespace jang {
+    class Program {
+        static void Main(string[] args) {
+            System.Console.WriteLine("Hello World!");
+            TestFunc.Test();
+        }
+    }
+
+    class TestFunc {
+        public static void Test() {
+            System.Console.WriteLine("Test".Pastel(Color.FromArgb(255, 0, 0)));
+        }
+    }
+}
